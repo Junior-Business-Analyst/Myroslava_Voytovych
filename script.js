4eb3d97d37ad004f6544bc030547a97972d61d20
Myroslava Voitovych <mamrosjvoit@gmail.com>
2026-05-11 19:47:15 +0300

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const burgerMenu = document.getElementById("burgerMenu");
 const leftPanel = document.getElementById("leftPanel");
 const closePanel = document.getElementById("closePanel");
 
-// Відкрити панель (ставимо в нуль, щоб вона виїхала зліва)
 burgerMenu.addEventListener("click", (e) => {
-    e.preventDefault(); // щоб сторінка не стрибала вгору
-    leftPanel.style.transform = "translateX(0)";
+    e.preventDefault();
+    leftPanel.classList.add("open"); // Додаємо клас, який ставить translateX(0)
 });
 
-// Закрити панель (ховаємо назад вліво на всю її ширину)
 closePanel.addEventListener("click", () => {
-    leftPanel.style.transform = "translateX(-100%)";
+    leftPanel.classList.remove("open"); // Прибираємо клас, повертається translateX(-100%)
 });
