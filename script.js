73c652362c9aed0b13a9c5e0692ebe657cf73594
Myroslava Voitovych <mamrosjvoit@gmail.com>
2026-05-11 19:53:51 +0300

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const burgerMenu = document.getElementById("burgerMenu");
 const leftPanel = document.getElementById("leftPanel");
 const closePanel = document.getElementById("closePanel");
 
-// Відкрити
 burgerMenu.addEventListener("click", (e) => {
     e.preventDefault();
-    leftPanel.classList.add("active");
+    leftPanel.style.transform = "translateX(0)"; // Виїжджає в нуль
 });
 
-// Закрити
 closePanel.addEventListener("click", () => {
-    leftPanel.classList.remove("active");
+    leftPanel.style.transform = "translateX(-100%)"; // Ховається повністю вліво
 });
